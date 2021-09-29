ae089147c5b27d2253e10f2be3f70038685632e5
Rocktim Rajkumar <rajkumar.rocktim@gmail.com>
2021-09-29 19:18:09 +0530

    add consumer

---
+++ consumer.js
@@ -0,0 +1,20 @@
+const amqp = require('amqplib')
+
+const msg = { number: 23 }
+connect()
+async function connect() {
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+
+        channel.consume("jobs", message => {
+            console.log(message.content.toString())
+        });
+
+        console.log("Waiting for messages....");
+    }
+    catch (ex) {
+        console.error(ex)
+    }
+}
